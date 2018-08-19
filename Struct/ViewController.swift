239e00a78083981de4f87612e1a67819c52e119c
Wong Ngai <wonghongngai@wongs-macbook-pro.local>
2018-08-19 21:48:42 +0800

    Initial Commit

---
+++ Struct/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Struct
+//
+//  Created by Wong Hong Ngai on 19/8/18.
+//  Copyright © 2018 Wong Hong Ngai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
